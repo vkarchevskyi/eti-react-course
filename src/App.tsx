5069b439082c1c3706965f40de66d1178085fe31
Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>
2026-02-23 18:44:31 +0200

    Create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <>
-      Hello World
+      <Header />
+      <main>Hello World</main>
     </>
   );
 }
