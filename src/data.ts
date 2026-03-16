58c8047d6ca1b568b491b326be5746fd139c0ac0
Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>
2026-03-16 22:31:13 +0200

    feat: create a mock data
    Signed-off-by: Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>

---
+++ src/data.ts
@@ -0,0 +1,37 @@
+export const mockData = [
+    {
+        "student": "Карчевський Володимир",
+        "mark": 90,
+        "active": true
+    },
+    {
+        "student": "Владислав Денисенко",
+        "mark": 80,
+        "active": true
+    },
+    {
+        "student": "Литвин Анастасія",
+        "mark": 90,
+        "active": true
+    },
+    {
+        "student": "Євлахович Ігор",
+        "mark": 100,
+        "active": true
+    },
+    {
+        "student": "Зінін Максим",
+        "mark": 98,
+        "active": true
+    },
+    {
+        "student": "Войдюк Ігор",
+        "mark": 60,
+        "active": true
+    },
+    {
+        "student": "Іван Петрович",
+        "mark": 69,
+        "active": false
+    },
+];
